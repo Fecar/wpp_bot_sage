95bbd9b39fda94dc0693fde9188a17424c62e53a
Felipe Caetano <fecae95@gmail.com>
2026-06-26 15:49:40 -0300

    feat: auto response test

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, request, jsonify
 
+from services.waha import Waha
+
 app = Flask(__name__)
 
 
-@app.route('/chatbot/webhook', methods=['POST'])
+@app.route('/chatbot/webhook/', methods=['POST'])
 def webhook():
     data = request.json
 
     print(f'EVENTO RECEBIDO: {data}')
+
+    waha = Waha()
+
+    chat_id = data['payload']['from']
+
+    waha.send_message(
+        chat_id=chat_id,
+        message='Ola'
+    )
 
     return jsonify({'status': 'success'}), 200
 
